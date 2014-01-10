3d18e3397b15944bad3059c800678d778055d18a
Heriberto Perez <heriberto.perez@crowdint.com>
2014-01-10 17:35:22 -0600

    Implement loadfromtemplate jquery plugin to be able to organize handlebars views

--- js/main.js
+++ js/main.js
@@ -1,3 +1,40 @@
 $(function () {
-  alert('hello');
+  //var source = $("#test_template").html();
+  //var template = Handlebars.compile(source);
+  var data = {
+    users: [ {
+        person: {
+            firstName: "Garry",
+            lastName: "Finch"
+        },
+        jobTitle: "Front End Technical Lead",
+        twitter: "gazraa"
+    }, {
+        person: {
+            firstName: "Garry",
+            lastName: "Finch"
+        },
+        jobTitle: "Photographer",
+        twitter: "photobasics"
+    }, {
+        person: {
+            firstName: "Garry",
+            lastName: "Finch"
+        },
+        jobTitle: "LEGO Geek",
+        twitter: "minifigures"
+    } ]
+  };
+  Handlebars.registerHelper('fullName', function(person) {
+    return person.firstName + " " + person.lastName;
+  });
+
+  //$('.body_container').append(template(data));
+  $('.body_container').html('').loadFromTemplate({
+    template : "test2",
+    data : data,
+    render_method: 'html',
+    extension : ".html",
+    path: 'templates/'
+  });
 });
---
+++ js/utils.js
@@ -0,0 +1,27 @@
+//Handlebars.getTemplate = function(name) {
+  //$.ajax({
+    //url : 'templates/' + name + '.handlebars',
+    //success : function(data) {
+      //console.log(data);
+      ////source    = data;
+      ////template  = Handlebars.compile(source);
+      ////$('#target').html(template);
+      //if (Handlebars.templates === undefined) {
+        //Handlebars.templates = {};
+      //}
+      //Handlebars.templates[name] = Handlebars.compile(data);
+    //},
+    //async : false
+  //});
+  //return Handlebars.templates[name];
+//};
+//
+//$.ajax({
+  //url: 'templates/hello.handlebars', //ex. js/templates/mytemplate.handlebars
+  //cache: true,
+  //success: function(data) {
+    //source    = data;
+    //template  = Handlebars.compile(source);
+    //$('.body_container').html(template);
+  //}
+//});
